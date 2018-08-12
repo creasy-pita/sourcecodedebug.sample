ab0b3e0eadf6825742c80e8484fc2bccc1f40ae0
creasy-pita <hz_ljq@live.com>
2018-08-12 16:37:18 +0800

    Initial commit

---
+++ CustomizeMiddleware/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CustomizeMiddleware
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            IWebHostBuilder webHostBuilder = WebHost.CreateDefaultBuilder(args);
+            webHostBuilder = webHostBuilder.UseStartup<Startup>();
+            webHostBuilder = webHostBuilder.UseUrls("http://0.0.0.0:5001");
+            return webHostBuilder.Build();
+        }
+
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //        //.UseUrls("http://192.168.174.128:5000")
+        //        .UseUrls("http://0.0.0.0:5001")
+        //        .Build();
+    }
+}
---
+++ CustomizeMiddleware/readme.txt
@@ -0,0 +1,7 @@
+﻿测试aspnetcore 启动流程的源码
+
+主要加入 
+Microsoft.AspNetCore.Hosting 
+Microsoft.AspNetCore.Hosting.Abstractions
+Microsoft.AspNetCore.Hosting.Server.Abstractions
+Microsoft.Extensions.Hosting.Abstractions
